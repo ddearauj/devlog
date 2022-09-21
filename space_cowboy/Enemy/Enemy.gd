a4598afc2edc3c6b3abc1b4d1f0494dce65a3943
Pereira Daniel <pereira.daniel@bcg.com>
2022-09-20 21:56:49 -0300

    added navigation to enemy. removed the initial ones from scene

--- space_cowboy/Enemy/Enemy.gd
+++ space_cowboy/Enemy/Enemy.gd
@@ -1,29 +1,44 @@
 extends KinematicBody2D
 
-
 var speed = 50
-
+var velocity = Vector2.ZERO
 var path: Array = []
 var levelNav: Navigation2D = null
 var player = null
 
+onready var line2d = $Line2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	yield(get_tree(), "idle_frame")
+	var tree = get_tree()
+	if tree.has_group("LevelNavigation"):
+		levelNav = tree.get_nodes_in_group("LevelNavigation")[0]
+	if tree.has_group("SpaceCowboy"):
+		player = tree.get_nodes_in_group("SpaceCowboy")[0]
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	line2d.global_position = Vector2.ZERO
+	if player and levelNav:
+		generate_path()
+		navigate()
 	move(delta)
 
 
-func move(delta):
-	var velocity = Vector2(0, speed)
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-#		emit_signal("door_colission")
-		pass
-	else:
-		velocity = move_and_slide(velocity)
+func navigate():
+	if path.size() > 0:
+		velocity = global_position.direction_to(path[1]) * speed
+		
+	if global_position == path[0]:
+		path.pop_front()
 	
-	return velocity
+func generate_path():
+	if player != null and levelNav != null:
+		path = levelNav.get_simple_path(global_position, player.global_position, false)
+		line2d.points = path
+
+func move(delta):
+	velocity = move_and_slide(velocity)
